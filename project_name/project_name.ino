f3750a90737de08487262433bc7089959ab5d918
Castanier Romain <castanier@insa-20386>
2025-12-03 12:10:29 +0100

    Premier commit - test Arduino

--- project_name/project_name.ino
+++ project_name/project_name.ino
@@ -1,15 +1,35 @@
+#include <Ultrasonic.h>
+
 #include "Application.h"
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#define SONIC_PIN D3
+#define LED_PIN D4
+
 
 Application myApplication;
+
+
 
 void setup() 
 {
   // put your setup code here, to run once:
-  myApplication.init();
+  //myApplication.init();
+  Ultrasonic(SONIC_PIN);
+  pinMode(LED_PIN, OUTPUT);
+
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  myApplication.run();
+  //myApplication.run();
+
+  if(MeasureInCentimeters(500)== 10000)
+    digitalWrite(LED_PIN, HIGH);
+  else
+    digitalWrite(LED_PIN, LOW);
+ 
 }
