9c9dce9ec27701ef49bbe7c3a47896a05b20c623
Bendib Juliette <pandipanda@MacBookAirdunePotterhead.local>
2026-01-15 14:34:08 +0100

    [Comments] Ajout de commentaire pour WebServerController et DFPlayerMini

--- project_name/DFPlayerMini.h
+++ project_name/DFPlayerMini.h
@@ -6,17 +6,55 @@
 class DFPlayerMini
 {
 public:
+
+    /**
+     * @brief  Constructeur de DFPlayerMini
+     *
+     * @details
+     * -
+     *
+     * @param param1 serial : Association d'un port série
+     */
     DFPlayerMini(HardwareSerial &serial);
 
-
+    /**
+    * @brief  Lancement d'un son stocké sur carte SD
+    *
+    * @details
+    * - Envoie commande lecture son + n° de la track à jouer
+    */
     void play(uint16_t track);
+
+    /**
+    * @brief  Coupe la track en cours d'éxécution
+    *
+    * @details
+    * - Envoie commande arret du son
+    */
     void stop();
+
+    /**
+    * @brief  Régler son de haut-parleur
+    *
+    * @details
+    * - Envoie commande réglage son + limitation du volume max à 30
+    */
     void setVolume(uint8_t volume);
+
+        /**
+    * @brief  Reset du DFPlayerMini
+    *
+    * @details
+    * - Envoie commande reset
+    */
     void reset();
 
 private:
     HardwareSerial &_serial;
 
+    /**
+    * @brief  Envoie trame de commande à DFPlayerMini sur liaison UART
+    */
     void send(uint8_t cmd, uint16_t param);
 };
 
--- project_name/WebServerController.h
+++ project_name/WebServerController.h
@@ -13,12 +13,41 @@
 
 class WebServerController {
 public:
+    /**
+     * @brief  Constructeur de WebServerController
+     *
+     * @details
+     * -
+     *
+     * @param param1 ledPin : branchement de led interne à ESP
+     * @param param2 rgbMatrix : référence sur class matriceRGB
+     * @param param3 motor : référence sur class VibrationMotor
+     * @param param4 lightSensor : référence sur class LightSensor
+     */
     WebServerController(matriceRGB &rgbMatrix,
                         VibrationMotor &motor,
                         LightSensor &light,
                         DFPlayerMini &player);
 
+    /**
+    * @brief  Initialisation de WebServerController
+    *
+    * @details
+    * - Initialisation de la Pin en OUTPUT et mise à état bas
+    * - Connexion Serial pour afficher retour info sur terminal PC
+    * - Connexion Wifi entre ESP8266 et Téléphone utilisateur 
+    * - Initialisation de matriceRGB
+    * - Activation de toutes les fonctions d'interruptions de page HTML pour contrôler capteurs/actionneurs
+    * - Lancement du serveur
+    */
     void begin(const char* ssid, const char* password);
+
+    /**
+     * @brief  Traitement des requêtes sur page HTML
+     *
+     * @details
+     * - Gestion de l'interruption à contrôler selon choix utilisateur sur page HTML
+     */
     void handle();
 
 private:
@@ -35,11 +64,53 @@ private:
 
     bool modeAuto = false;
 
+    /**
+     * @brief  Contrôle la MAJ de la page HTML
+     *
+     * @details
+     * - Met à jour la page HTML
+     */
     void handleRoot();
+
+    /**
+     * @brief  Contrôle état de la led Interne de l'ESP
+     *
+     * @details
+     * - Changement état led Interne
+     */
     void handleToggle();
+
+    /**
+     * @brief  Contrôle de l'animation sur matriceRGB
+     *
+     * @details
+     * - Lancement animation ou non selon mode sélectionné (Auto/Manuel)
+     */
     void handleRGB_Animation();
+
+    /**
+     * @brief  Contrôle le timer d'interruption du capteur LightSensor
+     *
+     * @details
+     * - Lancement de routine d'interruption pour capteur LightSensor selon mode (Auto/Manuel)
+     */
     void handleLightMode();
+
+    /**
+     * @brief  Contrôle le choix du son à jouer sur le DFPMiniplayer
+     *
+     * @details
+     * - Récupération du son à jouer de la page HTML 
+     * - Sélection du son à jouer
+     */
     void handleSound();
+
+    /**
+     * @brief  Changement état des boutons de page HTML
+     *
+     * @details
+     * - Changement état des attributs booléen lors de demande utilisateur
+     */
     String pageHTML();
 };
 
